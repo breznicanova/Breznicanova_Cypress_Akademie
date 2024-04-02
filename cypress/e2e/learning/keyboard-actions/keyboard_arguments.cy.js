0dc3a7466ee3e85d138b40e8332f8f18b164e451
Adriana Brezničanová <adbr@adbr-NTB.praha.tmapy.cz>
2024-04-02 20:59:27 +0200

    atomic tests

--- cypress/e2e/learning/keyboard-actions/keyboard_arguments.cy.js
+++ cypress/e2e/learning/keyboard-actions/keyboard_arguments.cy.js
@@ -2,6 +2,7 @@ describe("Keyboard arguments", () => {
   it("Confirm form by pressing enter key", () => {
     cy.visit("http://tredgate.com/pmtool/index.php?module=users/login");
     cy.get("#username").type("fifka_petr");
+    // cy.get("#password").type(Cypress.env("pmtool_password") + "{enter}");
     cy.get("#password").type("Tredgate2023{enter}");
   });
 });
--- cypress/page-objects/pmtool/users_page.js
+++ cypress/page-objects/pmtool/users_page.js
@@ -2,8 +2,8 @@ import { HeaderSection } from "./common/header_section";
 
 export class UserPage extends HeaderSection {
   constructor() {
-    super();
+    super("module=items/items&path=1");
     this.pageTitle = "h3.page-title";
-    cy.get(this.pageTitle).should("contain.text", "Users");
+    // cy.get(this.pageTitle).should("contain.text", "Users");
   }
 }
